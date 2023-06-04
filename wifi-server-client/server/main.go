91b9459e399be0377bb5451e9af0a0dd1fb9f812
Rohith <rohithsuresh207@gmail.com>
2023-06-04 10:32:59 +0530

    wifi-server-working

--- wifi-server-client/server/main.go
+++ wifi-server-client/server/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"net"
-  "fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func handleError(err error) {
@@ -11,54 +15,84 @@ func handleError(err error) {
 	}
 }
 
-func do(conn net.Conn) {
-	buf := make([]byte, 1024)
-	_, err := conn.Read(buf)
+type Client struct {
+	client_id int
+	conn      net.Conn
+}
+
+func sendMessage(client_id int, conn net.Conn, coordinates [4]string) {
+	fmt.Println([]byte(coordinates[client_id]))
+	num, err := conn.Write([]byte(coordinates[client_id]))
+	fmt.Println("num: ", num)
 	handleError(err)
 }
 
-type Client struct {
-  client_id int
-  conn net.Conn
+func broadCast(listOfClients []Client, coordinates [4]string) {
+	fmt.Println("broadcasting")
+	fmt.Println(listOfClients)
+	for _, client := range listOfClients {
+		fmt.Println(coordinates[client.client_id])
+		sendMessage(client.client_id, client.conn, coordinates)
+	}
 }
 
+func handleCommand(commandChannel chan string, coordinates [4]string) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter your input")
+	message, err := reader.ReadString('\n')
+	handleError(err)
 
-func sendMessage(client_id int,conn net.Conn,coordinates [4]string){
-  conn.Write([]byte(coordinates[client_id]))
+	message = strings.TrimSpace(message)
+
+	if message == "broadcast" {
+		broadCast(listOfClients, coordinates)
+	} else {
+		id, err := strconv.Atoi(message)
+		handleError(err)
+
+		var client Client
+		for _, c := range listOfClients {
+			if c.client_id == id {
+				client = c
+			}
+		}
+
+		sendMessage(id, client.conn, coordinates)
+	}
 }
 
-func broadCast(listOfClients []Client,coordinates [4]string){
-  for _,client := range listOfClients {
-    fmt.Println(coordinates[client.client_id])
-    go sendMessage(client.client_id,client.conn,coordinates)
-  }
-}
+var listOfClients []Client
 
 func main() {
 	listener, err := net.Listen("tcp", ":8080")
-  var listOfClients []Client
-  var coordinates = [...]string{
-    "1,1,1",
-    "2,2,2",
-    "3,3,3",
-    "4,4,4,4",
-  }
+	var coordinates = [...]string{
+		"1,1,1\n",
+		"2,2,2\n",
+		"3,3,3\n",
+		"4,4,4,4\n",
+	}
 	handleError(err)
-  fmt.Println("TCP server running at port 8080")
+	fmt.Println("TCP server running at port 8080")
 	defer listener.Close()
-  counter := 0
+
+	counter := 0
+	commandChannel := make(chan string)
+
+	go func() {
+		for {
+			handleCommand(commandChannel, coordinates)
+		}
+	}()
+
 	for {
 		conn, err := listener.Accept()
-    fmt.Println("Connected to client: number of clients ",len(listOfClients))
-    listOfClients = append(listOfClients,Client{
-      client_id: counter,
-      conn: conn,
-    })
-    counter+=1
-    if(len(listOfClients) == 4){
-      broadCast(listOfClients,coordinates)
-    }
+		newClient := Client{
+			client_id: counter,
+			conn:      conn,
+		}
+		listOfClients = append(listOfClients, newClient)
+		fmt.Println("Connected to client: number of clients ", len(listOfClients))
+		counter += 1
 		handleError(err)
-    go do(conn)
 	}
 }
